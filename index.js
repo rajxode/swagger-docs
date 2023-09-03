477532dcb734c5f8882746d20136e99b5c87cdcb
rajxode <rajsh1202@gmail.com>
2023-09-04 01:06:13 +0530

    create a route and added swagger docu for the same, with auth and tags

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.get('/',(req,res) => {
     res.send('<h1>Hello welcome in new Swagger project</h1>');
 })
 
+app.get('/api/v1/greet',(req,res) => {
+    res.send('greeting from Swagger');
+})
+
 
 app.listen(1000,() => {
     console.log("server is running on port : 1000");
