06ed47f80f30d526b515805323d8d97760711148
rajxode <rajsh1202@gmail.com>
2023-09-04 12:18:20 +0530

    create a dummy array , add a route for returning the array and write the swagger documentation for the array

--- index.js
+++ index.js
@@ -10,13 +10,32 @@ const swaggerDocument = YAML.load('./swagger.yaml')
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
+let array = [
+    {
+        id:"11",
+        name:"HTML and CSS",
+        price:499,
+    },
+    {
+        id:"22",
+        name:"JavaScript",
+        price:599,
+    },
+    {
+        id:"33",
+        name:"Reactjs",
+        price:699
+    },
+]
+
+
 app.get('/',(req,res) => {
     res.send('<h1>Hello welcome in new Swagger project</h1>');
-})
+});
 
 app.get('/api/v1/greet',(req,res) => {
     res.send('greeting from Swagger');
-})
+});
 
 app.get('/api/v1/object',(req,res) => {
     res.send({
@@ -24,8 +43,11 @@ app.get('/api/v1/object',(req,res) => {
         name:"object",
         description:"returning an object"
     });
-})
+});
 
+app.get('/api/v1/array',(req,res) => {
+    res.send(array);
+});
 
 app.listen(1000,() => {
     console.log("server is running on port : 1000");
